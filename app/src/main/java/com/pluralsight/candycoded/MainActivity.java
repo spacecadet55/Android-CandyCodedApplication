7bdac490ee565ed7f17cc355b2a69c3b6fe22688
meyere <71792859+meyere@users.noreply.github.com>
2021-06-08 15:28:52 -0400

    Update MainActivity.java

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -29,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
   private CandyDbHelper candyDbHelper = new CandyDbHelper(this);
 
   @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
