7821cf241f45b5b16a7252d0c4a7781e1c02f2a9
meyere <71792859+meyere@users.noreply.github.com>
2021-06-08 15:46:54 -0400

    Update MainActivity.java

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+  public boolean onOptionsItemSelected(MenuItem item) {
     Intent infoIntent = new Intent(this, InfoActivity.class);
     startActivity(infoIntent);
     return super.onOptionsItemSelected(item);
