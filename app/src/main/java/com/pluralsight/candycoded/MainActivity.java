988a648778faf47777f7668f5006c707964f1f64
spacecadet55 <>
2021-06-09 12:18:36 -0400

    Solution 1

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -73,6 +73,13 @@ public class MainActivity extends AppCompatActivity {
         });
   }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+    return super.onOptionsItemSelected(item);
+  }
+
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
